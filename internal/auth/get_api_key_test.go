dc8b27b62cabaa493480499963daa60835fbff91
IgnasJo@gmail.com <IgnasJo@gmail.com>
2026-07-19 16:59:03 +0300

    .

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -1,9 +1,9 @@
 package auth
 
 import (
+	"github.com/google/go-cmp/cmp"
 	"net/http"
 	"testing"
-	"github.com/google/go-cmp/cmp"
 )
 
 type Expected struct {
@@ -12,20 +12,20 @@ type Expected struct {
 }
 
 func TestGetAPIKey(t *testing.T) {
-    tests := map[string]struct {
-        input 		http.Header
-        want  	  Expected
-    }{
-        "normal":   {input: http.Header{"Authorization": []string{"ApiKey hello"}}, want: Expected{Val: "hello", Err: nil}},
-    }
+	tests := map[string]struct {
+		input http.Header
+		want  Expected
+	}{
+		"normal": {input: http.Header{"Authorization": []string{"ApiKey hello"}}, want: Expected{Val: "hello", Err: nil}},
+	}
 
-    for name, tc := range tests {
-        t.Run(name, func(t *testing.T) {
-            got, err := GetAPIKey(tc.input)
-            diff := cmp.Diff(tc.want, Expected{Val: got, Err: err})
-            if diff != "" {
-                t.Fatalf(diff)
-            }
-        })
-    }
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := GetAPIKey(tc.input)
+			diff := cmp.Diff(tc.want, Expected{Val: got, Err: err})
+			if diff != "" {
+				t.Fatalf(diff)
+			}
+		})
+	}
 }
